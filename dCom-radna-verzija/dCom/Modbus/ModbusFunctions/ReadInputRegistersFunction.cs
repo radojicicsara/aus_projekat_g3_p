9aaaefeee8cd54c8acb9ba27d55e115b145e4fc1
Sara Radojičić <sararadojicic.sri@gmail.com>
2026-04-22 19:24:32 +0200

    Periodicno ocitavanje i osvezavanje analognih ulaza i izlaza

--- dCom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
+++ dCom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -25,14 +25,60 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] ret_val = new byte[12];
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.TransactionId)), 0, ret_val, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.ProtocolId)), 0, ret_val, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.Length)), 0, ret_val, 4, 2);
+
+            ret_val[6] = CommandParameters.UnitId;
+            ret_val[7] = CommandParameters.FunctionCode;
+            Buffer.BlockCopy(
+                BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).StartAddress)),
+                0, ret_val, 8, 2);
+
+            Buffer.BlockCopy(
+                BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).Quantity)),
+                0, ret_val, 10, 2);
+
+            return ret_val;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> r = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            if (response[7] != CommandParameters.FunctionCode + 0x80)
+            {
+                ushort start_address = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+                int count = 0;
+                int byte_count = response[8];
+                ushort value = 0;
+
+                for (int i = 0; i < byte_count; i += 2)
+                {
+                    value = BitConverter.ToUInt16(response, 9 + i);
+                    value = (ushort)IPAddress.NetworkToHostOrder((short)value);
+
+                    r.Add(new Tuple<PointType, ushort>(PointType.ANALOG_INPUT, start_address), value);
+
+                    count++;
+                    start_address++;
+
+                    ushort quantity = ((ModbusReadCommandParameters)CommandParameters).Quantity;
+
+                    if (quantity <= count)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                HandeException(response[8]);
+            }
+            return r;
         }
     }
 }
+  
