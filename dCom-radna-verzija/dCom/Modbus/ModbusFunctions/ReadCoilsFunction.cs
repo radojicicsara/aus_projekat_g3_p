efbfe3fdaf7226315e3cfeb107de1719b34b33a4
Sara Radojičić <sararadojicic.sri@gmail.com>
2026-04-22 19:09:15 +0200

    Periodicno ocitavanje i osvezavanje digitalnih izlaza (Coils)

--- dCom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ dCom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -25,14 +25,59 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] ret_val = new byte[12];
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.TransactionId)), 0, ret_val, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.ProtocolId)), 0, ret_val, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.Length)), 0, ret_val, 4, 2);
+
+            ret_val[6] = CommandParameters.UnitId;
+            ret_val[7] = CommandParameters.FunctionCode;
+            Buffer.BlockCopy(
+                BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).StartAddress)),
+                0, ret_val, 8, 2);
+
+            Buffer.BlockCopy(
+                BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).Quantity)),
+                0, ret_val, 10, 2);
+
+            return ret_val;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> r = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            if (response[7] != CommandParameters.FunctionCode + 0x80)
+            {
+                ushort start_address = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+                int count = 0;
+                int byte_count = response[8];
+                int mask = 1;
+                for (int i = 0; i < byte_count; i++)
+                {
+                    byte temp = response[9 + i];
+                    for (int j = 0; j < 8; j++)
+                    {
+                        ushort value = (ushort)(temp & mask);
+                        r.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, start_address), value);
+                        temp >>= 1;
+                        count++;
+                        start_address++;
+                    }
+                    ushort quantity = ((ModbusReadCommandParameters)CommandParameters).Quantity;
+                    if (quantity <= count)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                HandeException(response[8]);
+            }
+            return r;
         }
     }
 }
+    
